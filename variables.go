b681967cb20c6660adc008982176442152478f8d
Maik Brauer <github@brauer.nrw>
2023-11-27 21:44:17 +0100

    fixed scope definition.

--- variables.go
+++ variables.go
@@ -4,11 +4,11 @@ import "os"
 
 const baseURL = "https://login.microsoftonline.com/"
 const tokenPath = "/oauth2/v2.0/token"
-const scope = "openid profile offline_access user.read"
 
 var TokenURL = baseURL + os.Getenv("tenant_id") + tokenPath
 var ClientSecret = os.Getenv("client_secret")
 var ClientID = os.Getenv("client_id")
+var scope = ClientID + "/.default offline_access"
 
 type Tokens struct {
 	AccessToken  string `json:"access_token"`
